5c20e317e52262eaa94feb2d857ea4bbec7484cd
CloudyCity <cloudycity@foxmail.com>
2019-12-21 13:32:00 +0800

    src增加文件分类,修改eslint文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -24,6 +24,27 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-unused-vars":
+      ["error", {
+        // 检查全局范围的变量
+        "vars": "all",
+        // 这允许函数使用两个命名参数
+        "args": "after-used"
+      }],
+    // 关闭不能使用tab
+    "no-tabs": 'off',
+    // 语句强制分号结尾
+    "semi": [0],
+    // 文件以单一的换行符结束
+    'eol-last': [0],
+    // 禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": [0],
+    // 函数定义时括号前面要不要有空格
+    'space-before-function-paren': [0],
+    // 缩进风格
+    'indent': [0],
+    // 空行上的尾随空白
+    'no-trailing-spaces': [0]
   }
 }
